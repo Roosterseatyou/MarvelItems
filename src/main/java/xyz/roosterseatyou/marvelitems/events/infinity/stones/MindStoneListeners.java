d6b3cd79c07946c122474e3c9ef5d4626cd6d0e2
Roosterseatyou <ltbkthree@icloud.com>
2022-06-02 16:15:43 -0500

    Start work on (very unstable) mind stone stuff.

---
+++ src/main/java/xyz/roosterseatyou/marvelitems/events/infinity/stones/MindStoneListeners.java
@@ -0,0 +1,33 @@
+package xyz.roosterseatyou.marvelitems.events.infinity.stones;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import xyz.roosterseatyou.marvelitems.MarvelItems;
+import xyz.roosterseatyou.marvelitems.api.enums.StoneType;
+import xyz.roosterseatyou.marvelitems.mobgoals.MindStoneControl;
+import xyz.roosterseatyou.marvelitems.utils.MarvelUtils;
+
+import java.util.ArrayList;
+
+public class MindStoneListeners implements Listener {
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractAtEntityEvent e) {
+        Player p = e.getPlayer();
+        if (!MarvelUtils.isWearingInfGauntlet(p)) return;
+        if (!(e.getRightClicked() instanceof Mob)) return;
+        Mob m = (Mob) e.getRightClicked();
+        ArrayList<StoneType> types = MarvelUtils.getStonesInGauntlet(p.getInventory().getChestplate());
+        p.sendMessage(Component.text("Stones you have: " + types.toString()));
+        if(types.isEmpty()) return;
+        if(types.contains(StoneType.MIND_STONE)) {
+            MindStoneControl control = new MindStoneControl(m, p);
+            Bukkit.getMobGoals().addGoal(m, 1, control);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(MarvelItems.getInstance(), () -> m.setKiller(p), 20 * 60);
+        }
+    }
+}
